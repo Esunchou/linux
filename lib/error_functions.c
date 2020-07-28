2903458e5218a4b6447ca54a65f2f32af72adc33
zhouyaqing <zhouyq@git.com>
2020-07-28 15:12:20 +0800

    zhouyaqing

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -113,19 +113,19 @@ errExit(const char *format, ...)
 }
 
 /* Display error message including 'errno' diagnostic, and
- *    terminate the process by calling _exit().
+ * terminate the process by calling _exit().
  *
- *       The relationship between this function and errExit() is analogous
- *          to that between _exit(2) and exit(3): unlike errExit(), this
- *             function does not flush stdout and calls _exit(2) to terminate the
- *                process (rather than exit(3), which would cause exit handlers to be
- *                   invoked).
+ * The relationship between this function and errExit() is analogous
+ * to that between _exit(2) and exit(3): unlike errExit(), this
+ * function does not flush stdout and calls _exit(2) to terminate the
+ * process (rather than exit(3), which would cause exit handlers to be
+ * invoked).
  *
- *                      These differences make this function especially useful in a library
- *                         function that creates a child process that must then terminate
- *                            because of an error: the child must terminate without flushing
- *                               stdio buffers that were partially filled by the caller and without
- *                                  invoking exit handlers that were established by the caller. */
+ * These differences make this function especially useful in a library
+ * function that creates a child process that must then terminate
+ * because of an error: the child must terminate without flushing
+ * stdio buffers that were partially filled by the caller and without
+ * invoking exit handlers that were established by the caller. */
 
 void
 err_exit(const char *format, ...)
